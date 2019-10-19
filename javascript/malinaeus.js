2e9bc1d7e639c11b3e93494189c2cb346c696956
Jackson Rittenhouse <rit19001@byui.edu>
2019-10-19 11:25:49 -0600

    added favicon

--- javascript/malinaeus.js
+++ javascript/malinaeus.js
@@ -1,1 +1,31 @@
-document.addEventListener("DOMContentLoaded", function(){let lastMod - document.lastModified; document.getElementById("modDate").innerText = lastMod;
+
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + d.getFullYear();
+
+document.getElementById("currentdate").textContent = fulldate;
